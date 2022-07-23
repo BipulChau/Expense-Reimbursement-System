2a0d2a1606691e2e35fe62dc0b84ca423c154fa8
Bipul Chaudhary <bipulchau@gmail.com>
2022-07-22 23:26:07 -0400

    Pytest-Mock test_user_login_negative

--- test/users_test.py
+++ test/users_test.py
@@ -19,3 +19,17 @@ def test_user_login_positive(mocker):
 
     # Assert
     actual == User("bach21", "Bach", "Tran", "employee")
+
+
+def test_user_login_negative(mocker):
+    # Arrange
+    def mock_user_login(username, password):
+        if username == "bipul513" and password == "password":
+            return User("bach21", "Bach", "Tran", "employee")
+        raise LogInError("Username and Password does not match. Please try again with correct credentials !!!")
+
+    mocker.patch("dao.users_dao.UsersDao.user_login", mock_user_login)
+
+    # ACT & Assert
+    with pytest.raises(LogInError) as e:
+        actual = UserService.user_login("bipul513", "apple")
