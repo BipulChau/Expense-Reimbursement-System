1a97b8995c4c8d8e860504eeed11fe6a2683f96d
Bipul Chaudhary <bipulchau@gmail.com>
2022-07-27 19:48:52 -0400

    Unit testing get_user_reimbursement_args

--- test/reimbursements_test.py
+++ test/reimbursements_test.py
@@ -32,3 +32,33 @@ def test_get_user_reimbursement(mocker):
             }
             ]
     }
+
+def test_get_user_reimbursement_args(mocker):
+    #  Arrange
+    def mock_get_user_reimbursement_args(user_id, args):
+        if user_id == "shaquera7" and args == "denied":
+            return [[7, 200.00, "Tue, 26 Jul 2022 15:42:47 GMT", "Wed, 27 Jul 2022 15:52:16 GMT", 'denied', 'Lodging', 'Foof', None, 'shaquera7', 'bipul513']]
+
+    mocker.patch("dao.reimbursement_dao.ReimbursementDao.get_user_reimbursement_args", mock_get_user_reimbursement_args)
+
+    # ACT
+    actual = ReimbursementService.get_user_reimbursement_args("shaquera7", "denied")
+
+    # Assert
+    var = actual == {
+        "Reimbursement details of shaquera7": [
+            {
+                "description": "Foof",
+                "receipt_img": None,
+                "reimb_author": "shaquera7",
+                "reimb_id": 7,
+                "reimb_resolver": "bipul513",
+                "reimbursement_amount": "200.00",
+                "resolved_at": "Wed, 27 Jul 2022 15:52:16 GMT",
+                "status": "denied",
+                "submitted_at": "Tue, 26 Jul 2022 15:42:47 GMT",
+                "type_of_expense": "Lodging"
+            }
+        ]
+    }
+
